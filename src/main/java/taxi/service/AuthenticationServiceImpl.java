41c992413c5f88262f56a16a40e6babbc80cbcec
Volodimir <vova.stepanenko2020@gmail.com>
2021-12-15 20:37:08 +0200

    add log4j

--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -5,9 +5,12 @@ import taxi.exception.AuthenticationException;
 import taxi.lib.Inject;
 import taxi.lib.Service;
 import taxi.model.Driver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
     @Inject
     private DriverService driverService;
 
@@ -15,8 +18,10 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     public Driver login(String login, String password) throws AuthenticationException {
         Optional<Driver> driver = driverService.findByLogin(login);
         if (driver.isPresent() && driver.get().getPassword().equals(password)) {
+            logger.info("Authentication was successful. Params: login={}", login);
             return driver.get();
         }
+        logger.warn("Authentication attempt failed. Params: login={}", login);
         throw new AuthenticationException("Username or password incorrect");
     }
 }
